95a54ff36e7ef2929d374ff31f32178fe1144c5d
YakupKursat <karahocagilyakup@outlook.com>
2021-08-06 10:10:37 +0300

    Navbar States

--- my-app/src/Components/NavBar.tsx
+++ my-app/src/Components/NavBar.tsx
@@ -3,7 +3,13 @@ import {Navbar, Nav, Container} from "react-bootstrap"
 import {useState} from "react"
 
 function NavBar() {
-  const[sayi,setSayi] = useState(0);
+  const[sayi,setSayi] = useState(1);
+
+  function clicked(){
+    setSayi(1);
+  }
+  console.log("sayı",sayi);
+
   return (
     <Navbar collapseOnSelect style={{position: "sticky"}} expand="sm" bg="dark" variant="dark" fixed="top" id="NavBarEdited">
       <Container>
@@ -14,9 +20,9 @@ function NavBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link id= {sayi === 1 ? "navLinkActive": "navLinkDisable"} href="/">Home</Nav.Link>
-            <Nav.Link id="navLink" href="/notespage">Notes</Nav.Link>
-            <Nav.Link id="navLink" href="#About">About</Nav.Link>            
+            <Nav.Link onClick={clicked}  id= {sayi === 1 ? "navLinkActive": "navLinkDisable"} href="/">Home</Nav.Link>
+            <Nav.Link onClick={clicked}  id= {sayi === 1 ? "navLinkActive": "navLinkDisable"} href="/notespage">Notes</Nav.Link>
+            <Nav.Link onClick={clicked}  id= {sayi === 1 ? "navLinkActive": "navLinkDisable"} href="#About">About</Nav.Link>            
           </Nav>
           <Nav>
             <Nav.Link id="navLink" href="#login">Login</Nav.Link>
